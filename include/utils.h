abf0860b9fe970751f516a0963b13b4731f8ec14
ArthurLVT <70861780+arthurlvt@users.noreply.github.com>
2025-11-07 09:25:41 +0100

    Implement utility functions for password hashing and verification

---
+++ include/utils.h
@@ -0,0 +1,9 @@
+#include <stdbool.h>
+
+#ifndef UTILS_H
+#define UTILS_H
+
+unsigned long hashPassword(const char *password, unsigned long salt);
+bool verifyPassword(const char *inputPassword, unsigned long storedHash, unsigned long salt);
+
+#endif
